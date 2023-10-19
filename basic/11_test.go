c8007ac38e65db97cce1a96cd119bd2fc550a424
DamperHa <1186962425@qq.com>
2023-10-19 14:12:08 +0800

    basic

--- basic/11_test.go
+++ basic/11_test.go
@@ -1,6 +1,39 @@
 package basic
 
+import (
+	"fmt"
+	"testing"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
 // 尽量定义零值可用的类型
 // C语言竟然没有对变量自动赋0值，而是任意值；
 
 // C语言中，我们首先需要对变量进行初始化操作。
+
+type FinishDecorationMessage struct {
+	StuId          int `json:"stuId"`
+	BizId          int `json:"bizId"`
+	CourseId       int `json:"courseId"`
+	BatchId        int `json:"batchId"`        // 批次id
+	UnitId         int `json:"unitId"`         // 单元id
+	PlanId         int `json:"planId"`         // 场次id
+	DramaId        int `json:"dramaId"`        // 剧本id
+	StuCouId       int `json:"stuCouId"`       // 购课id
+	ChapterId      int `json:"chapterId"`      // 章id
+	ChapterLogicId int `json:"chapterLogicId"` // 章逻辑id
+
+	SectionId      string `json:"sectionId"`
+	SectionLogicId string `json:"sectionLogicId"`
+
+	UnlockType int `json:"unlockType"` // 解锁类型，1：解锁章，2：解锁场次
+
+	MetaData map[string]string `json:"metadata"`
+}
+
+func TestDemo1(t *testing.T) {
+	arg := FinishDecorationMessage{}
+	r, _ := jsoniter.MarshalToString(arg)
+	fmt.Println(r)
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/golang/protobuf v1.5.3
 	github.com/google/uuid v1.3.1
 	github.com/google/wire v0.5.0
+	github.com/json-iterator/go v1.1.12
 	github.com/olahol/melody v1.1.4
 	github.com/redis/go-redis/v9 v9.1.0
 	github.com/sirupsen/logrus v1.9.3
@@ -19,6 +20,8 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	golang.org/x/net v0.16.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
